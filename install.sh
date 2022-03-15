c58531648b29a77adbd1372bf0e30c64e5a74d79
MichaelHDesigns <43361618+MichaelHDesigns@users.noreply.github.com>
2022-03-14 20:05:10 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ mkdir -p $helpthehomeless_prefix
 # don't try to reinstall dashd if it's already installed
 if [ ! -f /opt/helpthehomeless/bin/helpthehomelessd ]; then
   git clone --depth 1 https://github.com/MichaelHDesigns/helpthehomelessd-installer.git
-  pushd helpthehomelessd-installer.sh
+ # pushd helpthehomelessd-installer.sh
     source install.sh
   popd
 else
